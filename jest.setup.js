c23b6f739d6ae41ed8814f8fc2a8a358458e995f
Naoki Mita <naoki.mita18@gmail.com>
2022-09-18 18:15:00 -0700

    chore: set env variables for unit testing

--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,6 @@ jest.setTimeout(30000);
 beforeEach(() => {
   jest.clearAllMocks();
 });
+
+process.env.TEMPMON_WATCH_DIRECTORY = "./test/src/**";
+process.env.TEMPMON_TEMPLATE_DIRECTORY = "./test/template";
